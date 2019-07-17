507083773e3ef74790e80ce3dce89dd1dea8eab6
sionelt <sltafengatoto@gmail.com>
2019-07-16 18:11:02 -0600

    implement global request instance with axios

---
+++ config/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  port: 3000,
+  swapi: {
+    endpoint: 'https://swapi.co/api/',
+  },
+}
---
+++ services/request.service.js
@@ -0,0 +1,8 @@
+const axios = require('axios')
+
+const config = require('../config')
+
+
+module.exports = axios.create({
+  baseURL: config.swapi.endpoint,
+})
